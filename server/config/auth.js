5d4e5bbc2e839af7400fdc21ad070726a3fcc2f1
todorm85 <todorbm85@gmail.com>
2016-01-07 10:25:02 +0200

    Add authentication error messages

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,31 +1,40 @@
 var passport = require('passport');
 
 module.exports = {
-    login: function(req, res, next) {
-        var auth = passport.authenticate('local', function(err, user) {
-            if (err) return next(err);
-            if (!user) {
-                res.send({success: false}); // TODO: not only AJAX
+    login: function (req, res, next) {
+        var auth = passport.authenticate('local', function (err, user) {
+            if (err) {
+                req.session.error = "wrong username or password";
+                return res.redirect('/login');
             }
 
-            req.logIn(user, function(err) {
-                if (err) return next(err);
-                res.redirect('/');
+            if (!user) {
+                req.session.error = "wrong username or password";
+                return res.redirect('/login');
+            }
+
+            req.logIn(user, function (err) {
+                if (err) {
+                    req.session.error = "wrong username or password";
+                    return res.redirect('/login');
+                }
+
+                return res.redirect('/');
             });
         });
 
         auth(req, res, next);
     },
-    logout: function(req, res, next) {
+    logout: function (req, res, next) {
         req.logout();
         res.redirect('/');
     },
-    isAuthenticated: function(req, res, next) {
+    isAuthenticated: function (req, res, next) {
         if (!req.isAuthenticated()) {
+            req.session.error = "You must login first.";
             res.status(403);
             res.redirect('/login');
-        }
-        else {
+        } else {
             next();
         }
     }
