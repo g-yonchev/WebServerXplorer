10fb03b4435609cf1bbae55e5de19656d13fe70c
todorm85 <todorbm85@gmail.com>
2016-01-12 21:58:16 +0200

    Refactor: extract user creation logic to service.

--- server/controllers/UsersController.js
+++ server/controllers/UsersController.js
@@ -1,4 +1,3 @@
-var encryption = require('../services/encryption');
 var users = require('../services/users');
 
 var CONTROLLER_NAME = 'users';
@@ -10,7 +9,7 @@ module.exports = {
     getRegister: function (req, res, next) {
         res.render(`${CONTROLLER_NAME}/register`);
     },
-    postRegister: function(req, res, next) {
+    postRegister: function (req, res, next) {
         var newUserData = req.body;
 
         if (newUserData.password != newUserData.confirmPassword) {
@@ -18,26 +17,23 @@ module.exports = {
             return res.redirect('/register');
         }
 
-        newUserData.salt = encryption.generateSalt();
-        newUserData.hashPass = encryption.generateHashedPassword(newUserData.salt, newUserData.password);
-
-        users.create(newUserData, function(err, user) {
-            if (err) {
+        users.create(newUserData)
+            .then(function (user) {
+                req.logIn(user, function (err) {
+                    if (err) {
+                        res.status(400);
+                        return res.send({
+                            reason: err.toString()
+                        });
+                    } else {
+                        res.redirect('/');
+                    }
+                });
+            }, function (err) {
                 req.session.error = 'Failed to register new user.' +
-                    ' Perhaps already registered.' + 
-                    '\r\nerror: ' + err.errmsg;
-                res.redirect('/');
-                return;
-            }
-
-            req.logIn(user, function(err) {
-                if (err) {
-                    res.status(400);
-                    return res.send({reason: err.toString()});
-                } else {
-                    res.redirect('/');
-                }
+                    ' Perhaps already registered.' +
+                    '<br />error: ' + err.errmsg;
+                return res.redirect('/');
             });
-        });
     },
 };
